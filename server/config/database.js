8fe26ac022fc570ddda4021c557b1d72edb0d59b
Tom Hennessy <tomlhennessy@outlook.com>
2024-12-12 13:17:04 +1000

    one-to-many relationship

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   development: {
     storage: process.env.DB_FILE,
--- server/db/models/musician.js
+++ server/db/models/musician.js
@@ -10,13 +10,20 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // Your code here
+      Musician.belongsTo(models.Band, { foreignKey: 'bandId' });
     }
   };
   Musician.init({
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
-    bandId: DataTypes.INTEGER
+    bandId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Bands',
+        key: 'id',
+      }
+    }
   }, {
     sequelize,
     modelName: 'Musician',
